884136c6ec7d5ddee65b09c016bd66926ab9d0c0
yangchristina <ychristina011@gmail.com>
2022-02-09 14:09:47 -0800

    requires, modifies and effects

---
+++ src/main/model/EnergyEntry.java
@@ -0,0 +1,39 @@
+package model;
+
+import java.util.Date;
+
+public class EnergyEntry {
+    private Date date;
+    private int energyLevel;
+
+    // REQUIRES: date earlier than current time, energy level [1,10]
+    // EFFECTS: constructs a user's energy level entry with the date for the entry and the energy level recorded
+    public EnergyEntry(Date date, int energyLevel) { //either Date date or String date
+        this.date = date; // or this.date = new Date(date); where date is type long
+        this.energyLevel = energyLevel;
+    }
+
+    // REQUIRES: neededHours > 0
+    // MODIFIES: this
+    // EFFECTS: adds a new task to the collection of tasks to be completed
+    public void editDate(String className, int neededHours) {
+        // stub
+    }
+
+    // REQUIRES: energyLevel is [1, 10]
+    // MODIFIES: this
+    // EFFECTS: changes value of energy level
+    public void editEnergyLevel(int energyLevel) {
+        this.energyLevel = energyLevel;
+    }
+
+    // EFFECTS: returns date of energy entry
+    public Date getDate() {
+        return date;
+    }
+
+    // EFFECTS: returns energy level
+    public int getEnergyLevel() {
+        return energyLevel;
+    }
+}
