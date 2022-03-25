c3e7f3b08da5d6a538f8e8ad089ee0ec2c6c700c
yangchristina <ychristina011@gmail.com>
2022-03-25 15:08:47 -0700

    deleted comment

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -6,9 +6,6 @@ import java.awt.*;
 // starts execution
 public class Main {
     public static void main(String[] args) {
-        LoggedOutGUI frame = new LoggedOutGUI();
-//        new UserListOperations();
+        new LoggedOutGUI();
     }
-
-
 }
